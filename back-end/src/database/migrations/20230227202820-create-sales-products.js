c2234b0a86c6be2b60a4a7e75af1ed2ce94ee7bd
Guilherme Garcia <garciaguig@gmail.com>
2023-02-27 17:33:36 -0300

    chore: Configura a migration de criação da tabela 'sales_products'

---
+++ back-end/src/database/migrations/20230227202820-create-sales-products.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('sales_products', {
+      saleId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'sale_id',
+        references: {
+          model: 'sales',
+          key: 'id',
+        },
+        // onDelete: 'CASCADE',
+        // onUpdate: 'CASCADE',
+      },
+      productId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'product_id',
+        references: {
+          model: 'products',
+          key: 'id',
+        },
+        // onDelete: 'CASCADE',
+        // onUpdate: 'CASCADE',
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      }
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    return queryInterface.dropTable('sales_products');
+  },
+};
