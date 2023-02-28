8304e8b964f9c14309a2894d2cd36e3e16a33073
Helton-Pereira <helton.bfsm@gmail.com>
2023-02-28 19:30:38 -0300

    Corrige erros de lint

--- back-end/src/api/services/user.service.js
+++ back-end/src/api/services/user.service.js
@@ -1,24 +1,23 @@
-const { User } = require('../../database/models');
-const { generateToken } = require('../utils/token')
 const md5 = require('md5');
+const { User } = require('../../database/models');
+const { generateToken } = require('../utils/token');
 
 const login = async (email, password) => {
   const user = await User.findOne({ where: { email } });
   if (!user) {
-    return { status: 404, message: 'Invalid email' }
+    return { status: 404, message: 'Invalid email' };
   }
 
   const hashPassword = md5(password);
 
-  const token = generateToken(email)
+  const token = generateToken(email);
 
-  const { name, role } = user.dataValues
+  const { name, role } = user.dataValues;
 
   if (hashPassword === user.dataValues.password) {
-    return { status: 200, token, name, role }
+    return { status: 200, token, name, role };
   }
-  return { status: 404, message: 'Invalid password' }
+  return { status: 404, message: 'Invalid password' };
 };
 
 module.exports = { login };
-
