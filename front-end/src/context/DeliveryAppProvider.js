42d168af5ccd6ce74e26f7d922945c154c6d8e4c
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-02-28 15:02:24 -0300

    feat: Cria o contextApi e o fornece para rota

---
+++ front-end/src/context/DeliveryAppProvider.js
@@ -0,0 +1,31 @@
+// import React, { useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+import DeliveryAppContext from './DeliveryAppContext';
+
+// import {
+//   recipesReducer,
+//   RECIPES_INITIAL_STATE,
+//   USER_INITIAL_STATE,
+//   userReducer } from '../services/reducers';
+
+export default function DeliveryAppProvider({ children }) {
+  // const [recipes, recipesDispatch] = useReducer(recipesReducer, RECIPES_INITIAL_STATE);
+  // const [user, userDispatch] = useReducer(userReducer, USER_INITIAL_STATE);
+
+  // const contextValue = {
+  //   recipes,
+  //   recipesDispatch,
+  //   user,
+  //   userDispatch,
+  // };
+  return (
+    <DeliveryAppContext.Provider value={ contextValue }>
+      {children}
+    </DeliveryAppContext.Provider>
+  );
+}
+
+DeliveryAppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
