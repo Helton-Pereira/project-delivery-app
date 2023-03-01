64f8e37997a2acfdf4b00f00a086ce962446fd16
Guilherme Garcia <garciaguig@gmail.com>
2023-03-01 19:50:55 -0300

    fix: Corrige o valor da PORT em .env e services (front)

--- front-end/src/services/requests.js
+++ front-end/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://${process.env.REACT_APP_HOSTNAME || 'localhost'}:${process.env.REACT_APP_BACKEND_PORT || '3003'}`,
+  baseURL: `http://${process.env.REACT_APP_HOSTNAME || 'localhost'}:${process.env.REACT_APP_BACKEND_PORT || '3001'}`,
 });
 
 export const setToken = (token) => {
