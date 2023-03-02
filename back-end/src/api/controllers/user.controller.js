acfcc5f057a12cd986947cf43ce394a0ae122635
Guilherme Garcia <garciaguig@gmail.com>
2023-03-02 14:52:22 -0300

    refactor: Altera as informações retornadas pelo Login (backend)

--- back-end/src/api/controllers/user.controller.js
+++ back-end/src/api/controllers/user.controller.js
@@ -1,13 +1,14 @@
 const userService = require('../services/user.service');
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  const { email: _email, password } = req.body;
 
-  const { status, message, token, name, role } = await userService.login(email, password);
+  const {status, email, name, role, token, message} = await userService.login(_email, password);
 
   if (status === 200) {
-    return res.status(status).json({ token, name, role });
-  } 
+    return res.status(status).json({ name, email, role, token});
+  }
+
   return res.status(status).json({ message });
 };
 
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -119,4 +119,3 @@ Login.propTypes = {
 };
 
 export default Login;
-//
