2bff29cc32c6757dd788acc119d5b552915cc694
Helton-Pereira <helton.bfsm@gmail.com>
2023-03-03 15:58:26 -0300

    correções de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -12,12 +12,10 @@ app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
-
 /* app.use('/login', loginRoute);
 
 app.use('/register', registerRoute); */
 
 app.use(routes);
-
 
 module.exports = app;
--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -6,13 +6,11 @@ const productRouter = require('./productRoute');
 const checkoutRoute = require('./checkoutRoute');
 const { validateToken } = require('../middlewares/validateToken');
 
-
 const routers = express.Router();
 
 routers.use('/login', loginRouter);
 routers.use('/register', registerRouter);
 routers.use('/customer/products', productRouter);
 routers.use('/customer/checkout', validateToken, checkoutRoute);
-
 
 module.exports = routers;
