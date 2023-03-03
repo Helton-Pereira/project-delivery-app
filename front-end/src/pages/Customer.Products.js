d1bf841d073a6a3acd62cffdfaa5a26a4279bfc3
JoyceTeofilo <joyceteofilo@gmail.com>
2023-03-03 13:48:52 -0300

    feat: Cria botão do carrinho (sem implementação do localStorage)

--- front-end/src/pages/Customer.Products.js
+++ front-end/src/pages/Customer.Products.js
@@ -1,19 +1,46 @@
-import { useState } from 'react';
+// import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import CardProducts from '../components/Card.Products';
 import NavBarCustomer from '../components/NavBar.Customer';
+// import { requestData } from '../services/requests';
+import mockProducts from './mockProducts';
 
-function CustomerProducts() {
+function CustomerProducts(props) {
   // const [products, setProducts] = useState([]);
-  const [products] = useState([]);
 
-  // const user = localStorage.getItem('user');
+  // Testando sem o localStorage
+  const cartProducts = [{ price: 2.20, quantity: 2 }, { price: 2.49, quantity: 4 }]; // mock
+  const totalCalculate = cartProducts.map((prod) => prod.price * prod.quantity);
+  const initialValue = 0;
+  const total = totalCalculate.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  );
+  const totalFloat = total.toFixed(Number(2));
+  const totalString = totalFloat.toString();
+  const totalProduct = totalString.replace(/\./, ',');
+  // ----------
+
+  const handleClickCart = (event) => {
+    event.preventDefault();
+    const { history } = props;
+    history.push('/customer/checkout');
+  };
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const { data } = await requestData('/customer/products');
+  //     console.log(data);
+  //     setProducts(data);
+  //   };
+  //   getProducts();
+  // }, []);
 
   return (
-    <>
+    <main>
       <NavBarCustomer />
-      {/* <NavBarCustomer name={ JSON.parse(user).name } /> */}
-      {/* <CardProducts /> */}
-      {products.map((product) => (
+
+      {mockProducts.map((product) => (
         <CardProducts
           key={ product.id }
           name={ product.name }
@@ -22,8 +49,30 @@ function CustomerProducts() {
           id={ product.id }
         />
       ))}
-    </>
+
+      <div>
+        <button
+          type="button"
+          data-testid="customer_products__button-cart"
+          className="button-cart"
+          disabled={ totalProduct < 1 }
+          onClick={ handleClickCart }
+        >
+          <span
+            data-testid="customer_products__checkout-bottom-value"
+          >
+            { `Ver carrinho: ${totalProduct}` }
+          </span>
+        </button>
+      </div>
+    </main>
   );
 }
+
+CustomerProducts.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default CustomerProducts;
