98a010e258e68ad61b9e3705767d87a546c127d5
Helton-Pereira <helton.bfsm@gmail.com>
2023-03-03 15:34:23 -0300

    cria middleware validateToken.js

---
+++ back-end/src/api/middlewares/validateToken.js
@@ -0,0 +1,21 @@
+const { verifyToken } = require('../utils/token');
+const { mapError } = require('../utils/errorMap');
+
+const validToken = (token) => {
+  if (!token) {
+    return { type: 'TOKEN_NOT_FOUND', message: 'Token not found' };
+  }
+  const user = verifyToken(token);
+  return user;
+};
+const validateToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const { type, message } = validToken(authorization);
+  if (type) {
+    return res.status(mapError(type)).json({ message });
+  }
+  req.user = message;
+  next();
+};
+
+module.exports = { validateToken };
--- back-end/src/api/routes/checkoutRoute.js
+++ back-end/src/api/routes/checkoutRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const saleController = require('../controllers/sale.controller');
+const { validateToken } = require('../middlewares/validateToken');
 
 const router = express.Router();
 
-router.post('/', saleController.createSale);
+router.post('/', validateToken, saleController.createSale);
 
 module.exports = router;
