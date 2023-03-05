e9d95790d384d1f8f7e1e2d48295eaf2be2629fe
Guilherme Garcia <garciaguig@gmail.com>
2023-03-04 22:47:05 -0300

    refactor: Adequa component Product Card ao novo gerenciamento de estado

--- front-end/src/components/Card.Products.js
+++ front-end/src/components/Card.Products.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import DeliveryAppContext from '../context/DeliveryAppContext';
 
 function CardProducts({ id, name, price, urlImage }) {
   const [quantity, setQuantity] = useState(0);
@@ -7,18 +8,67 @@ function CardProducts({ id, name, price, urlImage }) {
   const priceFloat = price.toFixed(Number(2));
   const priceString = priceFloat.toString();
   const priceProduct = priceString.replace(/\./, ',');
+  const { cart, setCart } = useContext(DeliveryAppContext);
 
-  //   localStorage.setItem("key","value");
+  useEffect(() => {
+    const item = cart.find((items) => items.id === id);
+    if (item) setQuantity(item.quantity);
+    else setQuantity(0);
+  }, []);
 
-  const increaseQuantity = () => { setQuantity(quantity + 1); };
+  const updateQuantityItem = (newQty) => cart
+    .map((items) => (items.id === id
+      ? Object.assign(items, { ...items, quantity: newQty })
+      : items));
 
-  const decreaseQuantity = () => {
-    if (quantity > 0) return setQuantity(quantity - 1);
+  const increaseQuantity = (increase) => {
+    const value = quantity + increase;
+    setQuantity(value);
+    // const storage = browserStorage.get('cart') || [];
+    if (quantity === 0) {
+      const newCart = [...cart, { id, name, price, quantity: 1 }];
+      // storage.push({ id, name, price, quantity: 1 });
+      setCart(newCart);
+    } else {
+      const newCart = updateQuantityItem(value);
+      // item.quantity = quantity + increase;
+      setCart(newCart);
+    }
+  };
+
+  const decreaseQuantity = (decrease) => {
+    const value = !quantity ? 0 : quantity - decrease;
+    setQuantity(value);
+    if (quantity) {
+      // let storage = browserStorage.get('cart') || [];
+      // const item = storage.find((items) => items.id === id);
+      if (quantity > 1) {
+        const newCart = updateQuantityItem(value);
+        setCart(newCart);
+      } else {
+        // storage = storage.filter((items) => items.id !== id);
+        const newCart = cart.filter((items) => items.id !== id);
+        setCart(newCart);
+      }
+    }
   };
 
   const handleQuantityInput = ({ target }) => {
     const { value } = target;
-    setQuantity(value);
+    const convertedValue = Number(value);
+    setQuantity(convertedValue);
+    const item = cart.find((items) => items.id === id);
+
+    if (!item) {
+      const newCart = [...cart, { id, name, price, quantity: convertedValue }];
+      setCart(newCart);
+    } else if (convertedValue >= 1) {
+      const newCart = updateQuantityItem(convertedValue);
+      setCart(newCart);
+    } else {
+      const newCart = cart.filter((items) => items.id !== id);
+      setCart(newCart);
+    }
   };
 
   return (
@@ -55,7 +105,7 @@ function CardProducts({ id, name, price, urlImage }) {
           type="button"
           data-testid={ `customer_products__button-card-rm-item-${id}` }
           disabled={ false }
-          onClick={ decreaseQuantity }
+          onClick={ () => decreaseQuantity(1) }
         >
           -
         </button>
@@ -71,7 +121,7 @@ function CardProducts({ id, name, price, urlImage }) {
         <button
           type="button"
           data-testid={ `customer_products__button-card-add-item-${id}` }
-          onClick={ increaseQuantity }
+          onClick={ () => increaseQuantity(1) }
         >
           +
         </button>
