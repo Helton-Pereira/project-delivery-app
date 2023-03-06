ca5fcc5686462cf68d3636b1b7a6a6ea6935e295
Guilherme Garcia <garciaguig@gmail.com>
2023-03-06 12:15:44 -0300

    feat: Adiciona a rota /customer/orders (frontend)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import DeliveryAppProvider from './context/DeliveryAppProvider';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CustomerProducts from './pages/Customer.Products';
+import CustomerOrders from './pages/Customer.Orders';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Redirect exact from="/" to="/login" />
         <Route exact path="/register" component={ Register } />
         <Route exact path="/customer/products" component={ CustomerProducts } />
+        <Route exact path="/customer/orders" component={ CustomerOrders } />
       </Switch>
     </DeliveryAppProvider>
   );
