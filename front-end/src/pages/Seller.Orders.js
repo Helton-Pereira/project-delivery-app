077963061e4fb6c020c650c58815d58b0c413a41
Guilherme Garcia <garciaguig@gmail.com>
2023-03-06 15:57:56 -0300

    feat: Estrutura a page Seller Orders (frontend)

---
+++ front-end/src/pages/Seller.Orders.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import CardSellerOrder from '../components/Card.Seller.Order';
+import NavBarCustomer from '../components/NavBar.Customer';
+// import api from '../services/requests';
+import useValidateAuth from '../hooks/useValidateAuth';
+import mockedOrders from '../utils/sellerOrdersMocks';
+
+function SellerOrders(props) {
+  const { history } = props;
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useState(false);
+
+  useValidateAuth(props, setAuth);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      // const data = await api.requestData('/seller/orders'); // Aguardando implementação da rota. Após isso, não esquecer de apagar o mock.
+      setOrders(mockedOrders);
+    };
+    getOrders();
+    console.log(auth); // Provisório, só para não dar erro no linter | auth será utilizado na tela de Loading
+  }, []);
+
+  return (
+    <main>
+
+      <NavBarCustomer />
+
+      {orders.map((order) => (
+        <CardSellerOrder
+          history={ history }
+          key={ order.id }
+          id={ order.id }
+          status={ order.status }
+          saleDate={ order.saleDate }
+          totalPrice={ Number(order.totalPrice) }
+          deliveryAddress={ order.deliveryAddress }
+          deliveryNumber={ order.deliveryNumber }
+        />
+      ))}
+
+    </main>
+  );
+}
+
+SellerOrders.propTypes = {
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default SellerOrders;
---
+++ front-end/src/utils/sellerOrdersMocks.js
@@ -0,0 +1,30 @@
+// NÃO ESQUECER DE APAGAR APÓS A IMPLEMENTAÇÃO DA ROTA!
+
+const mockedOrders = [
+  {
+    id: 1,
+    totalPrice: '23.80',
+    saleDate: '08/04/21',
+    status: 'PENDENTE',
+    deliveryAddress: 'Rua Irmãos Monteiro, Bairo Pedras',
+    deliveryNumber: '851',
+  },
+  {
+    id: 2,
+    totalPrice: '14.20',
+    saleDate: '08/04/21',
+    status: 'PREPARANDO',
+    deliveryAddress: 'Rua Vila Bela,  Bairro Gurupi',
+    deliveryNumber: '670',
+  },
+  {
+    id: 3,
+    totalPrice: '28.46',
+    saleDate: '07/04/21',
+    status: 'ENTREGUE',
+    deliveryAddress: 'Rua Sessenta e Dois, Bairro Maranguape II',
+    deliveryNumber: '533',
+  },
+];
+
+export default mockedOrders;
