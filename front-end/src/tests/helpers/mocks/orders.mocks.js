8130d2a72d310c87b7df73b39089028bd5faa92f
Guilherme Garcia <garciaguig@gmail.com>
2023-03-06 10:53:08 -0300

    test: Adiciona testes para page Costumer Orders (frontend)

---
+++ front-end/src/tests/helpers/mocks/orders.mocks.js
@@ -0,0 +1,33 @@
+const idElement = 'customer_orders__element-order-id-';
+const statusElement = 'customer_orders__element-delivery-status-';
+const dateElement = 'customer_orders__element-order-date-';
+const priceElement = 'customer_orders__element-card-price-';
+
+const allOrders = [
+  {
+    id: 1,
+    totalPrice: '23.80',
+    saleDate: '08/04/21',
+    status: 'PENDENTE',
+  },
+  {
+    id: 2,
+    totalPrice: ' 14.20',
+    saleDate: '08/04/21',
+    status: 'PREPARANDO',
+  },
+  {
+    id: 3,
+    totalPrice: '28.46 ',
+    saleDate: '07/04/21',
+    status: 'ENTREGUE',
+  },
+];
+
+export default {
+  idElement,
+  statusElement,
+  dateElement,
+  priceElement,
+  allOrders,
+};
