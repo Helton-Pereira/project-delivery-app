f97370ec036a966ee1a2c9e313c45d2a9c6ad4b2
JoyceTeofilo <joyceteofilo@gmail.com>
2023-03-06 15:28:35 -0300

    Commit Inicial: feat customer order datails (front)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,7 @@ import Register from './pages/Register';
 import CostumerCheckout from './pages/Customer.Checkout';
 import CustomerProducts from './pages/Customer.Products';
 import CustomerOrders from './pages/Customer.Orders';
+import CustomerOrderDatails from './pages/Customer.Order.Details';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Redirect exact from="/" to="/login" />
         <Route exact path="/register" component={ Register } />
         <Route exact path="/customer/checkout" component={ CostumerCheckout } />
-        <Route exact path="/customer/orders/:id" />
+        <Route exact path="/customer/orders/:id" component={ CustomerOrderDatails } />
         <Route exact path="/customer/products" component={ CustomerProducts } />
         <Route exact path="/customer/orders" component={ CustomerOrders } />
       </Switch>
