a3792ab3f36bf9a1cedae609ce6289c34e159875
Guilherme Garcia <garciaguig@gmail.com>
2023-03-07 21:44:38 -0300

    :tada: Commit inicial - Fix Token Login (frontend)

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -44,6 +44,7 @@ function Login(props) {
       setErrorMessage('');
       setUser({ name, email, role, token });
       setCart([]);
+      //
 
       history.push('/customer/products');
     } catch (error) {
