ea365475f105a9739459bcc5850d50bb1ab4fbd9
Guilherme Garcia <garciaguig@gmail.com>
2023-03-09 11:53:45 -0300

    fix: Add await p/ verifyToken em getSalesBySellerId (backend)

--- back-end/src/api/controllers/seller.controller.js
+++ back-end/src/api/controllers/seller.controller.js
@@ -4,10 +4,10 @@ const { verifyToken } = require('../utils/token');
 const getSalesBySellerId = async (req, res) => {
   const token = req.headers.authorization;
 
-  const checkToken = verifyToken(token);
+  const checkToken = await verifyToken(token);
   
   try {
-    const sales = await sellerService.getSalesBySellerId(checkToken.data.email);
+    const sales = await sellerService.getSalesBySellerId(checkToken.email);
     return res.status(200).json(sales);
   } catch (error) {
     res.status(500).json({ message: 'An error has occurred' });
